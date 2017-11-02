137df28d49f43af0b22d80246943de7c356fca47
Mirat <mailto:Miratminsk@gmail.com>
2017-11-01 21:29:50 -0500

    1.3 JavaDoc. Code convention[#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,8 +1,16 @@
-package ru.job4j;
+﻿package ru.job4j;
+
+/**
+* Класс для вывода строки "Hello world"
+* @param name Массив строк
+* @author     Mirat Rakhmanov
+* @since      01.11.2017
+* @version    1.0
+*/
 
 class Calculate {
 
 public static void main(String[] args) {
-          System.out.println("Hello world");     
+    System.out.println("Hello world");     
 	}
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for calculate task.
+
+* @author     Mirat Rakhmanov
+* @since      01.11.2017
+* @version    1.0
+*/
