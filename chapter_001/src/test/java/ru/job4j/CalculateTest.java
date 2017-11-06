a0a8fb6a5401c5e7ba68875cf3cceebaf7a16235
Mirat <mailto:Miratminsk@gmail.com>
2017-11-05 21:49:37 -0600

    2.1 Настройка IDEA [#30150]

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -7,21 +7,21 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test
-* @author     Mirat Rakhmanov
-* @since      02.11.2017
-* @version    1.0
-*/
+ * Test
+ * @author     Mirat Rakhmanov
+ * @since      02.11.2017
+ * @version    1.0
+ */
 public class CalculateTest {
-/**
-* Test echo.
-*/ 
+	/**
+	 * Test echo.
+	 */
 	@Test
 	public void whenTakeNameThenTreeEchoPlusName() {
 		String input = "Mirat Rakhmanov";
-		String expect = "Echo, echo, echo : Mirat Rakhmanov"; 
+		String expect = "Echo, echo, echo : Mirat Rakhmanov";
 		Calculate calc = new Calculate();
 		String result = calc.echo(input);
 		assertThat(result, is(expect));
-    }
+	}
 }
