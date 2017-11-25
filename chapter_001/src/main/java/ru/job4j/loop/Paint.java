3fc09a23203d92c887fc8827cdca61aedc3005c4
Mirat Rakhmanov <miratminsk@gmail.com>
2017-11-25 00:01:51 -0600

    4.4 Построить пирамиду в псевдографике

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -4,7 +4,7 @@ package ru.job4j.loop;
  * Class для построения пирамиды в псевдографике
  * @author     Mirat Rakhmanov
  * @since      24.11.2017
- * @version    1.0
+ * @version    2.0
  * Method paint принимает положительное целое число h, и возвращать пирамиду в псевдографике
  * @param n
  * @return пирамидa в псевдографике
@@ -14,50 +14,20 @@ public class Paint {
 
     public String piramid(int h) {
         StringBuilder builder = new StringBuilder();
-        int l = ((h * 2) - 1);
-        int m = h;
-        int a = 1;
-        int b = m;
-        int e = m;
-        int lanes = 1;
-        int k = 1;
-        while (lanes <= h) {
-            if (k == 1) {
-            if (a < b) {
-                builder.append(" ");
-                a++;
-            }
-            if (a >= b && a < m) {
-                builder.append("^");
-                a++;
-            }
-            if (a == m) {
-                b--;
-                a = 1;
-                k = 2;
-            }
-            }
-            if (k == 2) {
-                if (m <= e) {
+        for (int a = 0; a < h; a++) {
+            for (int b = 0; b < h * 2; b++) {
+                if (b > h - 2 - a && b < h + a) {
                     builder.append("^");
-                    m++;
-                }
-                if (m > e && m <= l) {
+                } else if (b < h * 2 - 1) {
                     builder.append(" ");
-                    m++;
                 }
-                if (m > l) {
-                    e++;
-                    m = h;
-                    builder.append(System.getProperty("line.separator"));
-                    lanes++;
-                    k = 1;
-                }
+            }
+            if (a < h - 1) {
+                builder.append(System.getProperty("line.separator"));
             }
         }
-            return builder.toString();
+        builder.append(System.getProperty("line.separator"));
+        return builder.toString();
     }
 }
-
-
 
