6a4125e42381b564a07c82dd1e254c429d08d484
Mirat Rakhmanov <miratminsk@gmail.com>
2018-04-16 20:56:14 -0500

    2. Очередь с приоритетом на LinkedList [#41670] v.3

--- chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -1,25 +1,22 @@
 package ru.job4j.search;
 
-import java.util.Iterator;
 import java.util.LinkedList;
 
 public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>();
 
     public void put(Task task) {
-        if (tasks.size() == 0) {
-            this.tasks.add(tasks.size(), task);
-        } else {
-            Iterator<Task> iter = tasks.iterator();
-            while (iter.hasNext()) {
-                Task str = iter.next();
-                    if (str.getPriority() > task.getPriority()) {
-                        this.tasks.add(tasks.indexOf(str), task);
-                        break;
-                    }
-                }
+        int size = this.tasks.size();
+        if (size == 0) {
+            this.tasks.add(task);
+        }
+        for (int index = 0; index != size; index++) {
+            if (tasks.get(index).getPriority() > task.getPriority()) {
+                this.tasks.add(index, task);
+                break;
             }
         }
+    }
     public Task take() {
         return this.tasks.poll();
     }
--- chapter_003/src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ chapter_003/src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -1,10 +1,12 @@
 package ru.job4j.search;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PriorityQueueTest {
+
     @Test
     public void whenHigherPriority() {
         PriorityQueue queue = new PriorityQueue();
@@ -14,7 +16,8 @@ public class PriorityQueueTest {
         queue.put(new Task("B", 2));
         queue.put(new Task("A", 1));
         queue.put(new Task("B", 2));
+        queue.put(new Task("Victory", 0));
         Task result = queue.take();
-        assertThat(result.getDesc(), is("A"));
+        assertThat(result.getDesc(), is("Victory"));
     }
 }
